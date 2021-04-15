62e2e3c2cc1b8da64f06db57a057991911e410fd
MountainCat1 <72751263+MountainCat1@users.noreply.github.com>
2021-04-15 05:38:12 +0200

    Initialize Zerg Merch Shop

---
+++ GridTest/testScript.js
@@ -0,0 +1,13 @@
+function DoThings() {
+    if(document.body.style.background == "black"){
+        document.body.style.background = "grey"
+    }
+    else
+    {
+        document.body.style.background = "black"
+    }
+
+
+    var myDiv = document.getElementById("kupa")
+    myDiv.textContent = "XDD";
+}
